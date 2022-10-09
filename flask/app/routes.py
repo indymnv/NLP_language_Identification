52c535363b8e82d4e3e0ff2caed4a5abfdf2f45b
PitHurtado <pitehrh@gmail.com>
2022-10-09 00:03:07 -0300

    Add index.html template

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -4,15 +4,11 @@ from flask import render_template
 from app import app #, podria importar APP si lo llego a ocupar en app.py
 
 @app.route('/',methods=['GET'])
-def index():
-  return render_template('index.html', titlle='Home')
-
-@app.route('/home',methods=['GET'])
 def home():
   return render_template('index.html', titlle='Home')
 
-@app.route('/about')
-def about():
-  return render_template('about.html', titlle='Home')
-
-# mas links pages
+@app.route('/identification', methods=['GET'])
+def runModel():
+  valores = "valore calculado"
+  return render_template('index.html', output = valores)
+  
